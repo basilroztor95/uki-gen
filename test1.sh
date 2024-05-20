2042f3e94108e8863234c573cef32830a216084d
Basil <43650215+basilroztor95@users.noreply.github.com>
2024-05-21 00:31:59 +0300

    Create test1.sh

---
+++ test1.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+calculate_offsets() {
+    align_hex=$(objdump -p /usr/lib/systemd/boot/efi/linuxx64.efi.stub | awk '/SectionAlignment/ {print $2}')
+    align=$((16#$align_hex))
+
+    cmdline_offs_hex=$(objdump -h "/usr/lib/systemd/boot/efi/linuxx64.efi.stub" | awk 'NF==7 {print strtonum("0x"$3) + strtonum("0x"$4)}')
+    cmdline_offs=$((cmdline_offs_hex + align - cmdline_offs_hex % align))
+
+    initrd_offs=$((cmdline_offs + $(stat -Lc%s "/boot/cmdline.txt")))
+    initrd_offs=$((initrd_offs + align - initrd_offs % align))
+
+    linux_offs=$((initrd_offs + $(stat -Lc%s "/boot/comb_initrd.img")))
+    linux_offs=$((linux_offs + align - linux_offs % align))
+
+    objcopy \
+    --add-section .cmdline="/boot/cmdline.txt"      --change-section-vma .cmdline=$(printf 0x%x $cmdline_offs) \
+    --add-section .initrd="/boot/comb_initrd.img"   --change-section-vma .initrd=$(printf 0x%x $initrd_offs) \
+    --add-section .linux="/boot/vmlinuz-linux"      --change-section-vma .linux=$(printf 0x%x $linux_offs) \
+    "/usr/lib/systemd/boot/efi/linuxx64.efi.stub" "/efi/EFI/Linux/linux.efi"
+}
+
+cat /boot/intel-ucode.img /boot/booster-linux.img > /boot/comb_initrd.img
+
+calculate_offsets
