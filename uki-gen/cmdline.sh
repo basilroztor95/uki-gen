d60910684b421c970c1e1d9aecc0af5581d50272
Basil <43650215+basilroztor95@users.noreply.github.com>
2024-05-31 22:37:17 +0300

    Update cmdline.sh

--- uki-gen/cmdline.sh
+++ uki-gen/cmdline.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-echo "rd.luks.uuid=$(cryptsetup luksUUID /dev/nvme0n1p2) root=UUID=$(blkid -s UUID -o value /dev/mapper/rootfs)" > /proc/cmdline
+echo "rd.luks.uuid=$(cryptsetup luksUUID /dev/nvme0n1p2) root=UUID=$(blkid -s UUID -o value /dev/mapper/rootfs)" > /boot/cmdline
